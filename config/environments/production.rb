db47ba3bd6653bab7fe0c41af904d4a6691ed952
enoch232 <enoch232@gmail.com>
2016-03-21 23:56:36 -0700

    fix for env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,8 +78,10 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.paperclip_defaults = {
+    
   :storage => :s3,
   :s3_credentials => {
+    :s3_region => ENV['AWS_REGION'],
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
